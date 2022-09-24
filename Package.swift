261fc9cf0ade343b91808e39ee30d1d494525c5a
Kerry Knight <kknight@butterflynetinc.com>
2022-09-24 15:09:57 -0400

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,51 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "LaunchDarkly",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "LaunchDarkly",
+            targets: ["LaunchDarkly"]),
+    ],
+    dependencies: [
+        .package(name: "DarklyEventSource", path: "../DarklyEventSource"),
+    ],
+    targets: [
+        .target(
+            name: "LaunchDarkly",
+            dependencies: [
+                .product( name: "DarklyEventSource", package: "DarklyEventSource"),
+            ],
+            path: "./",
+            exclude: [
+                "Cartfile",
+                "Cartfile.resolved",
+                "Carthage",
+                "Pods",
+                "Gemfile",
+                "Gemfile.lock",
+                "SourceryTemplates",
+                "Framework",
+                "LaunchDarkly.podspec",
+                "LaunchDarkly.xcodeproj",
+                "LaunchDarkly.xcworkspace",
+                "LaunchDarkly/GeneratedCode",
+                "LaunchDarkly/LaunchDarklyTests",
+                "LaunchDarkly/LaunchDarkly/Support"
+            ],
+            sources: [
+                "LaunchDarkly/LaunchDarkly/Extensions",
+                "LaunchDarkly/LaunchDarkly/Models",
+                "LaunchDarkly/LaunchDarkly/Networking",
+                "LaunchDarkly/LaunchDarkly/Service Objects",
+                "LaunchDarkly/LaunchDarkly/LDClient.swift",
+                "LaunchDarkly/LaunchDarkly/LDCommon.swift"
+            ]
+        )
+    ]
+)
